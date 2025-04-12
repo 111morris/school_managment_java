04b72b02f7bf53bd440ae466bad6b956174e842d
mulandi <mulandimorris1@gmail.com>
2025-04-12 22:37:46 +0300

    dvpkg

--- src/school_management_system/Student.java
+++ src/school_management_system/Student.java
@@ -3,4 +3,13 @@ package school_management_system;
 this class is responsible for keeping the track of students fees, name, grade and fee
  */
 public class Student {
+  private int age;
+  private String name;
+  private int grade;
+  private int feePaid;
+  private int feeTotal;
+
+  public Student(int id, String name, int grade){
+
+  }
 }
