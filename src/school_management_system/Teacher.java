b62c428b3eb6c636cd0c9d032b9741bc227aab68
mulandi <mulandimorris1@gmail.com>
2025-04-13 23:52:49 +0300

    updated the comment

--- src/school_management_system/Teacher.java
+++ src/school_management_system/Teacher.java
@@ -1,5 +1,7 @@
 package school_management_system;
-
+/*
+this clas is responsible for keeping the track of teacher's name, id, salary.
+ */
 public class Teacher {
     private int id;
     private String name;
