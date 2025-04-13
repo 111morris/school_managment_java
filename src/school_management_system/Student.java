b31b0a8750d48f9ef7cb35a648f86ec0bf003b57
mulandi <mulandimorris1@gmail.com>
2025-04-13 23:50:40 +0300

    dvpkg

--- src/school_management_system/Student.java
+++ src/school_management_system/Student.java
@@ -14,7 +14,7 @@ public class Student {
 
   public Student(int id, String name, int grade){
     this.feePaid = 0;
-    this.feeTotal = 30000;dit a
+    this.feeTotal = 30000;
     this.id = id;
     this.name = name;
     this.grade = grade;
