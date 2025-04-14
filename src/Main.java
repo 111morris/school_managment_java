d848a499998fdf966abb975ac58381c04d6e2f77
mulandi <mulandimorris1@gmail.com>
2025-04-14 19:35:57 +0300

    dvpgk

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,36 @@
+import school_management_system.School;
+import school_management_system.Student;
 import school_management_system.Teacher;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Teacher teacher = new Teacher(42402, "morris Mulandi", 700000);
-        System.out.println(teacher.getId());
-        System.out.println(teacher.getSalary());
-        int tempSalary = teacher.getSalary();
-        teacher.setSalary(800000);
-        System.out.println(teacher.getName() + " salary was changed from "+ tempSalary + " to " + teacher.getSalary());
+        Teacher lizzy =  new Teacher(001, "lizzy", 500);
+        Teacher mellisa = new Teacher(002, "Mellisa", 700);
+        Teacher kelvin = new Teacher(003, "Kelvin", 500);
+        Teacher munga = new Teacher(004, "Munga", 600);
+
+        List<Teacher> teacherList = new ArrayList<>();
+        teacherList.add(lizzy);
+        teacherList.add(mellisa);
+        teacherList.add(kelvin);
+        teacherList.add(munga);
+
+        Student tamasha = new Student(2000, "Tamasha", 4);
+        Student rash = new Student(2001, "rashid", 12);
+        Student mango = new Student(2002, "Mango", 10);
+
+        List<Student> studentList = new ArrayList<>();
+        studentList.add(tamasha);
+        studentList.add(rash);
+        studentList.add(mango);
+
+        School kmc = new School(teacherList, studentList);
+
+
+        System.out.println(kmc.getTotalMoneyEarned());
     }
+
 }
