874ded362407839947557eedf18d14317cd64d01
mulandi <mulandimorris1@gmail.com>
2025-04-14 18:45:36 +0300

    added school java class

--- src/school_management_system/School.java
+++ src/school_management_system/School.java
@@ -1,4 +1,12 @@
 package school_management_system;
 
+/**
+ * the school will have several teachers and students
+ * ArrayList which implements teacher and student using an ArrayList
+ */
 public class School {
+    private Teacher teacher;
+    private Student student;
+    private int totalMoneyEarned;
+    private int totalMoneySpent;
 }
